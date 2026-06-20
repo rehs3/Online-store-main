36147c81022fee49aa4d6d3b5fa2040c019fca86
rehs23 <reginaalves@alu.ufc.br>
2026-06-20 19:36:03 -0300

    escrita422

--- static/admin/js/SelectFilter2.js
+++ static/admin/js/SelectFilter2.js
@@ -3,29 +3,22 @@
     globalThis.SelectFilter = {
         init: function(field_id, field_name, is_stacked) {
             if (field_id.match(/__prefix__/)) {
-                // Don't initialize on empty forms.
                 return;
             }
             const from_box = document.getElementById(field_id);
-            from_box.id += '_from'; // change its ID
+            from_box.id += '_from'; 
             from_box.className = 'filtered';
             for (const p of from_box.parentNode.getElementsByTagName('p')) {
                 if (p.classList.contains("info")) {
-                    // Remove <p class="info">, because it just gets in the way.
                     from_box.parentNode.removeChild(p);
                 } else if (p.classList.contains("help")) {
-                    // Move help text up to the top so it isn't below the select
-                    // boxes or wrapped off on the side to the right of the add
-                    // button:
                     from_box.parentNode.insertBefore(p, from_box.parentNode.firstChild);
                 }
             }
 
-            // <div class="selector"> or <div class="selector stacked">
             const selector_div = quickElement('div', from_box.parentNode);
             selector_div.className = is_stacked ? 'selector stacked' : 'selector';
 
-            // <div class="selector-available">
             const selector_available = quickElement('div', selector_div);
             selector_available.className = 'selector-available';
             const title_available = quickElement('h2', selector_available, interpolate(gettext('Available %s') + ' ', [field_name]));
@@ -58,7 +51,6 @@
             const choose_all = quickElement('a', selector_available, gettext('Choose all'), 'title', interpolate(gettext('Click to choose all %s at once.'), [field_name]), 'href', '#', 'id', field_id + '_add_all_link');
             choose_all.className = 'selector-chooseall';
 
-            // <ul class="selector-chooser">
             const selector_chooser = quickElement('ul', selector_div);
             selector_chooser.className = 'selector-chooser';
             const add_link = quickElement('a', quickElement('li', selector_chooser), gettext('Choose'), 'title', gettext('Choose'), 'href', '#', 'id', field_id + '_add_link');
@@ -66,7 +58,6 @@
             const remove_link = quickElement('a', quickElement('li', selector_chooser), gettext('Remove'), 'title', gettext('Remove'), 'href', '#', 'id', field_id + '_remove_link');
             remove_link.className = 'selector-remove';
 
-            // <div class="selector-chosen">
             const selector_chosen = quickElement('div', selector_div);
             selector_chosen.className = 'selector-chosen';
             const title_chosen = quickElement('h2', selector_chosen, interpolate(gettext('Chosen %s') + ' ', [field_name]));
@@ -89,7 +80,6 @@
 
             from_box.name = from_box.name + '_old';
 
-            // Set up the JavaScript event handlers for the select box filter interface
             const move_selection = function(e, elem, move_func, from, to) {
                 if (elem.classList.contains('active')) {
                     move_func(from, to);
@@ -138,16 +128,15 @@
             });
             SelectBox.init(field_id + '_from');
             SelectBox.init(field_id + '_to');
-            // Move selected from_box options to to_box
             SelectBox.move(field_id + '_from', field_id + '_to');
+            
             if (!is_stacked) {
-    
                 const j_from_box = document.getElementById(field_id + '_from');
                 const j_to_box = document.getElementById(field_id + '_to');
                 let height = filter_p.offsetHeight + j_from_box.offsetHeight;
                 const j_to_box_style = globalThis.getComputedStyle(j_to_box);
+                
                 if (j_to_box_style.getPropertyValue('box-sizing') === 'border-box') {
-                   
                     height += Number.parseInt(j_to_box_style.getPropertyValue('padding-top'), 10)
                         + Number.parseInt(j_to_box_style.getPropertyValue('padding-bottom'), 10)
                         + Number.parseInt(j_to_box_style.getPropertyValue('border-top-width'), 10)
@@ -157,11 +146,9 @@
                 j_to_box.style.height = height + 'px';
             }
 
-           
             SelectFilter.refresh_icons(field_id);
         },
         any_selected: function(field) {
-            
             field.required = true;
             const any_selected = field.checkValidity();
             field.required = false;
@@ -170,10 +157,8 @@
         refresh_icons: function(field_id) {
             const from = document.getElementById(field_id + '_from');
             const to = document.getElementById(field_id + '_to');
-            // Active if at least one item is selected
             document.getElementById(field_id + '_add_link').classList.toggle('active', SelectFilter.any_selected(from));
             document.getElementById(field_id + '_remove_link').classList.toggle('active', SelectFilter.any_selected(to));
-            // Active if the corresponding box isn't empty
             document.getElementById(field_id + '_add_all_link').classList.toggle('active', from.querySelector('option'));
             document.getElementById(field_id + '_remove_all_link').classList.toggle('active', to.querySelector('option'));
         },
