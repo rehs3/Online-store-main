c412f1476e12aa8b4336e2f458f97b306ed4a227
rehs23 <reginaalves@alu.ufc.br>
2026-06-20 19:09:21 -0300

    escrita418

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothing_store.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,7 +12,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )(exc)
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
